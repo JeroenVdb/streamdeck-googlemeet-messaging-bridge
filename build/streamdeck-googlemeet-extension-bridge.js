70d92bacef5ea13ea4b9d10a75fe60981b6f8e2f
Jeroen Van den Berghe <vandenberghe.jeroen@gmail.com>
2020-07-09 16:08:10 +0200

    fix bug + add logging

--- build/streamdeck-googlemeet-extension-bridge.js
+++ build/streamdeck-googlemeet-extension-bridge.js
@@ -3,26 +3,28 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 process.title = 'googlemeetbridge';
 const ws_1 = require("ws");
-const clients = [];
 const webSocketServer = new ws_1.Server({ port: 1987 });
+let clients = [];
 webSocketServer.on('connection', function connection(ws) {
-    ws.on('message', handleIncomingMessages);
-});
-function handleIncomingMessages(message) {
-    try {
-        const msg = JSON.parse(message);
-        if (msg.type === 'identify') {
-            clients.push(ws);
+    ws.on('message', function handleIncomingMessages(message) {
+        console.log(`Received message: ${message}`);
+        try {
+            const msg = JSON.parse(message);
+            if (msg.type === 'identify') {
+                clients.push(ws);
+            }
+            else {
+                broadcast(message);
+            }
         }
-        else {
+        catch (e) {
+            console.error(e);
             broadcast(message);
         }
-    }
-    catch (e) {
-        broadcast(message);
-    }
-}
+    });
+});
 function broadcast(message) {
+    console.log(`Send message to ${clients.length} clients: ${message}`);
     clients.forEach((client) => {
         client.send(message);
     });
--- src/streamdeck-googlemeet-extension-bridge.ts
+++ src/streamdeck-googlemeet-extension-bridge.ts
@@ -8,29 +8,33 @@ process.title = 'googlemeetbridge';
 
 import { Server } from 'ws';
 
-const clients: WebSocket[] = [];
 const webSocketServer = new Server({ port: 1987 });
 
+let clients: WebSocket[] = [];
+
 webSocketServer.on('connection', function connection(ws) {
-	ws.on('message', handleIncomingMessages);
-});
+	ws.on('message', function handleIncomingMessages(message: string) {
+		console.log(`Received message: ${message}`);
 
-function handleIncomingMessages(message: string) {
-	try {
-		const msg: any = JSON.parse(message);
+		try {
+			const msg: any = JSON.parse(message);
 
-		if (msg.type === 'identify') {
-			// @ts-ignore
-			clients.push(ws);
-		} else {
+			if (msg.type === 'identify') {
+				// @ts-ignore
+				clients.push(ws);
+			} else {
+				broadcast(message);
+			}
+		} catch (e) {
+			console.error(e);
 			broadcast(message);
 		}
-	} catch (e) {
-		broadcast(message);
-	}
-}
+	});
+});
 
 function broadcast(message: string) {
+	console.log(`Send message to ${clients.length} clients: ${message}`);
+
 	clients.forEach((client) => {
 		client.send(message);
 	});
